e5e5000941dce59d238370ae99080f25a97e6072
furusetomoya <tomo.fwork18@gmail.com>
2022-07-07 16:58:28 +0900

    deploy環境構築

--- src/pages/MindMap.tsx
+++ src/pages/MindMap.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Box, Input , Stack,HStack} from '@chakra-ui/react'
+import {
+  Box, Input, Stack, HStack,
+} from '@chakra-ui/react';
 
 function MindMap() {
   return (
     <div className="App">
       <h1>MindMap Page</h1>
-        <Box bg='tomato' w='100%' p={4} color='white'>
-          <Stack spacing={4}>
-            <Input placeholder='Basic usage' />
-            <HStack>
-                <Input placeholder='Basic usage' />
-                <Input placeholder='Basic usage' />
-            </HStack>
-            <HStack>
-                <Input placeholder='Basic usage' />
-                <Input placeholder='Basic usage' />
-                <Input placeholder='Basic usage' />
-                <Input placeholder='Basic usage' />
-            </HStack>
-          </Stack>
-        </Box>
+      <Box bg="tomato" w="100%" p={4} color="white">
+        <Stack spacing={4}>
+          <Input placeholder="Basic usage" />
+          <HStack>
+            <Input placeholder="Basic usage" />
+            <Input placeholder="Basic usage" />
+          </HStack>
+          <HStack>
+            <Input placeholder="Basic usage" />
+            <Input placeholder="Basic usage" />
+            <Input placeholder="Basic usage" />
+            <Input placeholder="Basic usage" />
+          </HStack>
+        </Stack>
+      </Box>
     </div>
   );
 }
