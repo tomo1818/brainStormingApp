1fc4522c3f26083cac864ebcb805fb63863b5f82
furusetomoya <tomo.fwork18@gmail.com>
2022-07-27 15:57:44 +0900

    テキストの更新

--- src/components/NodeUI.tsx
+++ src/components/NodeUI.tsx
@@ -1,5 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Box, Editable, EditableInput, EditableTextarea, EditablePreview, IconButton } from '@chakra-ui/react';
+/* eslint-disable object-curly-newline */
+import React, { useState, useRef } from 'react';
+import {
+  Box,
+  IconButton,
+  Input,
+} from '@chakra-ui/react';
 import { AddIcon, CloseIcon } from '@chakra-ui/icons';
 import Draggable, { DraggableEvent } from 'react-draggable';
 import { Node } from '../types/node';
@@ -17,17 +22,35 @@ type Props = {
 type Position = {
   xRate: number;
   yRate: number;
-}
+};
 type DraggableData = {
-  node: HTMLElement,
-  x: number, y: number,
-  deltaX: number, deltaY: number,
-  lastX: number, lastY: number
+  node: HTMLElement;
+  x: number;
+  y: number;
+  deltaX: number;
+  deltaY: number;
+  lastX: number;
+  lastY: number;
 };
 
 type DraggableEventHandler = (e: Event, data: DraggableData) => void | false;
 
 export function NodeUI({ item, addList, deleteList, updateList }: Props) {
+  const [text, setText] = useState(item.text);
+  const changeText = (value: string) => {
+    setText(value);
+  };
+  const updateText = (node: Node) => {
+    updateList(
+      node.id,
+      text,
+      node.color,
+      node.size,
+      node.parentId,
+      node.x,
+      node.y,
+    );
+  };
   const clickAddButton = () => {
     addList('addSample', '', '', item.id, item.x + 100, item.y + 100);
   };
@@ -96,11 +119,12 @@ export function NodeUI({ item, addList, deleteList, updateList }: Props) {
             y:
             {currentPosition.yRate}
           </span>
-          {item.text}
-          <Editable defaultValue="Take some chakra">
-            <EditablePreview />
-            <EditableInput />
-          </Editable>
+          <Input
+            borderColor="transparent"
+            value={text}
+            onChange={(e) => changeText(e.target.value)}
+            onBlur={() => updateText(item)}
+          />
           <IconButton
             borderRadius="full"
             aria-label="Search database"
